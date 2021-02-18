a440570afe53adeeaa0dd836388a249c1c6f006a
Sérgio Sobrinho <sobrinhosergio00@gmail.com>
2021-02-18 15:45:55 -0300

    arquivos bases adicionados

---
+++ helpers/url.php
@@ -0,0 +1,8 @@
+<?php
+
+    
+  $BASE_URL = "http://" . $_SERVER['SERVER_NAME'] . dirname($_SERVER['REQUEST_URI']. '?') . '/';
+  //Para que conseiga utilizar imagens e estilos
+
+
+?>
---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+    include_once("helpers/url.php");
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Codar</title>
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/style/main.css">
+    <!-- GOOGLE FONTS -->
+   <link rel="preconnect" href="https://fonts.gstatic.com">
+   <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
+   
+</head>
+<body>
+
+<h1>Olá, Mundo! </h1>
+    
+</body>
+</html>
