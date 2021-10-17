54bec3a3639195bad64307d263a55c348d7fa1b4
Sérgio de Souza Lima Sobrinho <53090956+SobrinhoSergio@users.noreply.github.com>
2021-10-17 11:51:04 -0300

    Update Header

--- template/header.php
+++ template/header.php
@@ -11,7 +11,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Codar</title>
-    <link rel="stylesheet" href="style/main.css">
+    <link rel="stylesheet" href="../style/main.css">
     <!-- GOOGLE FONTS -->
    <link rel="preconnect" href="https://fonts.gstatic.com">
    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&display=swap" rel="stylesheet">
